eefa8a82d75c4a0458cba4bfe8c0dbd610aa5b29
akumm2k <82381989+akumm2k@users.noreply.github.com>
2023-01-26 04:20:36 +0100

    Add javadoc to Main

--- src/main/java/org/reversi/Main.java
+++ src/main/java/org/reversi/Main.java
@@ -3,7 +3,21 @@ package org.reversi;
 import org.reversi.mvc.ReversiController;
 import org.reversi.mvc.ReversiModel;
 
+/**
+ * The Main class to run the game
+ */
 public class Main {
+    /**
+     * Forbidden constructor as Main is a utility class
+     */
+    public Main() {
+        throw new RuntimeException("Utility class can't be initialized.");
+    }
+
+    /**
+     * runs the game with an AI agent
+     * @param args CLI args
+     */
     public static void main(String[] args) {
         final ReversiModel model = new ReversiModel(8);
         final ReversiController controller = ReversiController.getInstance();
