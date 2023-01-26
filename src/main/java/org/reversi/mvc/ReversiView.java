e1c18cf53f6da73937611f5fba7a6d5933d90542
akumm2k <82381989+akumm2k@users.noreply.github.com>
2023-01-26 03:24:35 +0100

    Add javadoc to ReversiView

--- src/main/java/org/reversi/mvc/ReversiView.java
+++ src/main/java/org/reversi/mvc/ReversiView.java
@@ -2,42 +2,99 @@ package org.reversi.mvc;
 
 import java.util.Set;
 
+/**
+ * The view class of the MVC architecture for reversi
+ */
 public final class ReversiView {
 
+    /**
+     * a colored string to represent a possible / feasible move given a game state
+     */
     private final static String POSSIBLE_MOVE_TILE = String.format("%s%s%s", Color.YELLOW, "*", Color.RESET);
 
+    /**
+     * a string to represent an empty tile on the board
+     */
     private final static String EMPTY_TILE = "_";
 
+    /**
+     * strings to represent the player disks on the board
+     */
     public final static String[] PLAYER_TILES = {"x", "o"};
 
+    /**
+     * singleton view instance
+     */
     private final static ReversiView INSTANCE = new ReversiView();
 
+    /**
+     * private constructor for singleton behavior
+     */
     private ReversiView() {}
+
+    /**
+     * method to retrieve the singleton instance
+     * @return the singleton view instance
+     */
     public static ReversiView getInstance() {
         return INSTANCE;
     }
 
+    /**
+     * An enum to hold all the colors for displaying strings printed by the View
+     * using ANSI strings
+     * @see <a href="https://en.wikipedia.org/wiki/ANSI_escape_code">ANSI wikipedia</a>
+     */
     private enum Color {
+        /**
+         * turns off all ANSI attributes set so far, which should return the console to its defaults
+         */
         RESET("\033[0m"),
 
+        /**
+         * ANSI red
+         */
         @SuppressWarnings("unused")
         RED("\033[0;31m"),
+
+        /**
+         * ANSI yellow
+         */
         YELLOW("\033[0;33m"),
+
+        /**
+         * ANSI blue
+         */
         BLUE("\033[0;34m");
 
+        /**
+         * string holding the ANSI color code
+         */
         private final String colorCode;
+
+        /**
+         * The enum constructor
+         * @param colorCode string holding the ANSI color code
+         */
         Color(final String colorCode) {
             this.colorCode = colorCode;
         }
 
+        /**
+         * Overridden toString to directly access color code
+         * @return the enum's colorCode
+         */
         @Override
         public String toString() {
             return colorCode;
         }
     }
 
+    /**
+     * prints the current state of the board to the console
+     * @param gameState the game board to be printed
+     */
     public void printBoard(final ReversiModel gameState) {
-        // Print the current state of the board to the console
         final int[][] board = gameState.getBoard();
         final Set<Coordinate> possibleMoves = gameState.getPossibleMoves();
 
@@ -69,19 +126,29 @@ public final class ReversiView {
 
     }
 
+    /**
+     * prints out some introductory text
+     * @param exitKey key required for exiting the game, should the player want to
+     */
     public void welcome(final String exitKey) {
         System.out.println("Welcome to Яeversi");
         System.out.printf("Enter %s to exit%n", exitKey);
     }
 
+    /**
+     * prints the current player to the console
+     * @param currentPlayer the player whose turn it is to play
+     */
     public void printCurrentPlayer(final int currentPlayer) {
-        // Print the current player to the console
         final int playerID = ReversiModel.getPlayerIndex(currentPlayer);
         System.out.println("Turn: " + PLAYER_TILES[playerID]);
     }
 
+    /**
+     * prints out the current result of the game
+     * @param model carries the game state
+     */
     public void printResult(final ReversiModel model) {
-        // Print the winner of the game to the console
         if (model.getWinner() == ReversiModel.DRAW) {
             System.out.println("DRAW");
             return;
